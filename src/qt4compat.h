d2283df39723e542416a1fcb4fd292780fe690f5
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-01-22 00:28:37 +0200

    Use compiler detection for Q_LIKELY, Q_UNLIKELY, Q_ASSUME, Q_UNREACHABLE [ci skip]

--- src/qt4compat.h
+++ src/qt4compat.h
@@ -3,6 +3,24 @@
 
 #include <QtCore/QtGlobal>
 
+#if !defined(Q_UNREACHABLE) && !defined(Q_ASSUME)
+#	if defined(Q_CC_INTEL) || defined(Q_CC_MSVC)
+#		define Q_UNREACHABLE() __assume(0)
+#		define Q_ASSUME(s) __assume(s)
+#	elif defined(Q_CC_CLANG)
+#		define Q_UNREACHABLE() __builtin_unreachable()
+#	elif defined(Q_CC_GNU) && defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ * 100 + __GNUC_MINOR__) >= 405
+#		define Q_UNREACHABLE() __builtin_unreachable()
+#	endif
+#endif
+
+#if !defined(Q_LIKELY) && !defined(Q_UNLIKELY)
+#	ifdef __GNUC__
+#		define Q_LIKELY(expr)    __builtin_expect(!!(expr), true)
+#		define Q_UNLIKELY(expr)  __builtin_expect(!!(expr), false)
+#	endif
+#endif
+
 #ifndef Q_LIKELY
 #	define Q_LIKELY(s) s
 #endif
